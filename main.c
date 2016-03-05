85ecc3db211f4d6cb579d281ac65d3133a5cbc1c
zesilva63 <ze_dantas@hotmail.com>
2016-03-05 19:45:39 +0000

    adição do main.h e melhorias com comentarios no main.c

--- main.c
+++ main.c
@@ -1,39 +1,52 @@
 #include "main.h"
-#include "check_prods.h"
-
 
 int main() {
 
    char line[MAX_SIZE];
    char* informacao;
+
+   // arrays de strings onde serão guardados em memória os dados dos ficheiros txt
+   
    char clientes[16385][6];
    char produtos[171009][7];
-   
    int i = 0;
    
    FILE *fp;
-   
 
-   // COLOCA CLIENTES NUM ARRAY
+   // COLOCA CLIENTES NUM ARRAY (terá de ser mudado para uma arvore por eficiencia)
+   
    fp = fopen("Clientes.txt","r");
+   
    while(fgets(line,MAX_SIZE,fp)) {
       informacao = strtok(line,"\n\r");
       strcpy(clientes[i],informacao);
-      //printf("%s\n",clientes[i]);
       i++;
    }  
+   
    fclose(fp);
+
+   // COLOCA PRODUTOS NUM ARRAY (terá de ser mudado para uma arvore por eficiencia)
 
    i = 0;
    fp = fopen("Produtos.txt","r");
+   
    while(fgets(line,MAX_SIZE,fp)) {
       informacao = strtok(line,"\n\r");
       strcpy(produtos[i],informacao);
-      //printf("%s\n",produtos[i]);
       i++;
-   }  
-   printf("%s\n",produtos[0]);
+   }
+   
+   fclose(fp);
+
+   // VERIFICACAO DAS VENDAS (existência do cliente e do produto vendido -> arvore tambem)
+
+   /*
+   
+   **CODE**
+
+   */
 
    return 0;
 }
+
 
