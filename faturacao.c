a564d0704c784bb7adf717887c7162165ff47a33
zesilva63 <ze_dantas@hotmail.com>
2016-04-05 10:04:10 +0100

    commit anterior teve erros

---
+++ faturacao.c
@@ -0,0 +1,1 @@
+#include "faturacao.h"
---
+++ faturacao.h
@@ -0,0 +1,15 @@
+#ifndef _faturacao_h_
+#define _faturacao_h_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "clientes.h"
+#include "produtos.h"
+#include "avl.h"
+
+
+
+#endif
